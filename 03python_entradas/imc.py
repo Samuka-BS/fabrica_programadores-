61c19979e61f9297ec46347ab14169a59109b91e
Samuka-BS <samuelbsilva899@gmail.com>
2026-05-09 11:39:53 -0300

    python entradas

---
+++ 03python_entradas/imc.py
@@ -0,0 +1,9 @@
+# Autor: Samuel Barbosa
+# Projeto: Entrada pelo usuário
+
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+imc =  peso / (altura * altura)
+
+# exibir o resultado com variável
+print('O resultado é: ',imc)
